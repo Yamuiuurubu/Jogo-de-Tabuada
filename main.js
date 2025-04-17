c0a9d2df77fb778bd2d17922dee12bcf99b4ba07
Yamuiuurubu <iranyamamoto@gmail.com>
2025-04-17 16:06:36 -0300

    web version alpha

---
+++ main.js
@@ -0,0 +1,32 @@
+const numeros = [2,3,4,5,6,7,8,9]
+
+const respostaInput = document.getElementById("resposta")
+const perguntaElement = document.getElementById("pergunta")
+const mensagem = document.getElementById("mensagem")
+
+var x1,x2
+
+function gerar() {
+    i1 = Math.floor(Math.random() * numeros.length)
+    i2 = Math.floor(Math.random() * numeros.length)
+
+    x1 = numeros[i1]
+    x2 = numeros[i2]
+
+    perguntaElement.textContent = `Quanto é ${x1} x ${x2}?`
+}
+
+function enviar() {
+    var resposta_certa = x1*x2
+    var resposta = respostaInput.value
+
+    if(resposta == resposta_certa) {
+        respostaInput.value = ''
+        return gerar()
+    } else {
+        console.log("errou")
+        respostaInput.value = ''
+    }
+}
+
+gerar()
